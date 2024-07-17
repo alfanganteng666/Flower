abdd07527c0c20b7cbf949e04400ebfef12f0d04
m.alfan <114430531+alfanganteng666@users.noreply.github.com>
2024-07-18 00:39:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const errorMessage = document.getElementById("error-message");
+
+    // Daftar username yang diperbolehkan
+    const validUsername = "Salsabila Syandana Ananta".toLowerCase();
+
+    if (username === validUsername) {
+      // Redirect to landing page
+      window.location.href = "flwr.html";
+    } else {
+      errorMessage.textContent = "bukan salsa...";
+    }
+  });
